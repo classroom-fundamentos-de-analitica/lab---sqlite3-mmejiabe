7d2a0c70a81d123d474a7121c4be86b64c32d63c
mmejiabe <116604065+mmejiabe@users.noreply.github.com>
2022-11-28 13:39:49 -0500

    Update pregunta_04.sql

--- pregunta_04.sql
+++ pregunta_04.sql
@@ -40,4 +40,5 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
-SELECT K0;c16 FROM tbl1 WHERE SUBSTR(k0,1,1) = SUBSTR(c16,1,1);
+
+SELECT k0,c16 FROM tbl1 t WHERE SUBSTR(c16,1,1)=K0;
